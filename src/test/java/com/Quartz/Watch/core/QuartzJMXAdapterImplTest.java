4e3f6aab9b924b6245313391d45e50c926c3db40
java10000 <123363975@qq.com>
2013-01-14 11:59:32 +0800

    maven管理项目结构调整

--- src/test/java/com/Quartz/Watch/core/QuartzJMXAdapterImplTest.java
+++ src/test/java/com/Quartz/Watch/core/QuartzJMXAdapterImplTest.java
@@ -15,59 +15,69 @@ import com.quartz.monitor.object.QuartzInstance;
 import com.quartz.monitor.object.Scheduler;
 import com.quartz.monitor.util.JMXUtil;
 
-public class QuartzJMXAdapterImplTest {
+public class QuartzJMXAdapterImplTest
+{
 
-	QuartzConfig config = null;
-	QuartzInstance quartzInstance = null;
-	
-	@Before
-	public  void setUpBeforeClass() throws Exception {
-		 try {
-			 config = new QuartzConfig("11","127.0.0.1", 2911, "", "");
-			QuartzConnectService quartzConnectService = new QuartzConnectServiceImpl();
-			quartzInstance = quartzConnectService.initInstance(config);
-			System.out.println("------------");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	@Test
-	public void testGetVersion() {
-		fail("Not yet implemented");
-	}
+    QuartzConfig config = null;
 
-	@Test
-	public void testGetJobDetails() throws Exception {
-		
-		
-		List<Scheduler> schedulers = quartzInstance.getSchedulerList();
-		
-		System.out.println(schedulers.size());
-		JMXInput jmxInput = new JMXInput(quartzInstance, new String[] { String.class.getName() },
-				"AllJobDetails", new Object[] { schedulers.get(0).getUuidInstance() },schedulers.get(0).getObjectName());
-		
-		Object o1 = JMXUtil.callJMXAttribute(jmxInput);
-		Object o2 = JMXUtil.callJMXOperation(jmxInput);
-		System.out.println(o1);
-		System.out.println(o2);
-	}
+    QuartzInstance quartzInstance = null;
 
-	@Test
-	public void testGetScheduler() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void setUpBeforeClass() throws Exception
+    {
+        try
+        {
+            config = new QuartzConfig("11", "127.0.0.1", 2911, "", "");
+            QuartzConnectService quartzConnectService = new QuartzConnectServiceImpl();
+            quartzInstance = quartzConnectService.initInstance(config);
+            System.out.println("------------");
+        }
+        catch (Exception e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void testGetTriggersForJob() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetVersion()
+    {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testAttachListener() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetJobDetails() throws Exception
+    {
+
+        List<Scheduler> schedulers = quartzInstance.getSchedulerList();
+
+        System.out.println(schedulers.size());
+        JMXInput jmxInput = new JMXInput(quartzInstance, new String[]
+        { String.class.getName() }, "AllJobDetails", new Object[]
+        { schedulers.get(0).getUuidInstance() }, schedulers.get(0).getObjectName());
+
+        Object o1 = JMXUtil.callJMXAttribute(jmxInput);
+        Object o2 = JMXUtil.callJMXOperation(jmxInput);
+        System.out.println(o1);
+        System.out.println(o2);
+    }
+
+    @Test
+    public void testGetScheduler()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTriggersForJob()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAttachListener()
+    {
+        fail("Not yet implemented");
+    }
 
 }
